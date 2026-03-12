ab7988cd45549f27e2073f682ad22fe6964d1dbd
ardi-21 <ardiponte@gmail.com>
2026-03-12 09:10:53 -0500

    Create animales.java

---
+++ nombre_carpeta/animales.java
@@ -0,0 +1,73 @@
+public class Main {
+    class Animal {
+        void sonido() {
+            system.out.println(x:"El animal hace un sonido");
+        }
+        void moverse() {
+            system.out.println(x:"El animal se mueve");
+        }
+        void comer() {
+            system.out.println(x:"El animal come");
+        }
+    }
+}
+ 
+class perro extends animal {
+    voud sonido() {
+        system.out.println(x:"El perro ladra");
+    }
+    voud moverse() {
+        system.out.println(x:"El perro corre");
+    }
+    voud comer() {
+        system.out.println(x:"El perro come croquetas");
+    }
+}
+ 
+Class gato extends animal {
+    void sonido() {
+        system.out.println(x:"El gato maulla");
+    }
+    void moverse() {
+        system.out.println(x:"El gato salta");
+    }
+    void comer() {
+        system.out.println(x:"El gato come pescado");
+    }
+}
+ 
+Class pajaro extends animal {
+    void sonido() {
+        system.out.println(x:"El pajaro canta");
+    }
+    void moverse() {
+        system.out.println(x:"El pajaro vuela");
+    }
+    void comer() {
+        system.out.println(x:"El pajaro come semillas");
+    }
+}
+ 
+Class vaca extends animal {
+    void sonido() {
+        system.out.println(x:"La vaca muge");
+    }
+    void moverse() {
+        system.out.println(x:"La vaca camina");
+    }
+    void comer() {
+        system.out.println(x:"La vaca come pasto");
+    }
+}
+ 
+Class serpiente extends animal {
+    void sonido() {
+        system.out.println(x:"La serpiente sisea");
+    }
+    void moverse() {
+        system.out.println(x:"La serpiente se arrastra");
+    }
+    void comer() {
+        system.out.println(x:"La serpiente come ratones");
+    }
+}
